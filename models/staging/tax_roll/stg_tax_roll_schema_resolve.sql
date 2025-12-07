8065593a06339da03a7c6c8306e4bfe27e5bbb80
bnoffke <bnoffk3790@gmail.com>
2025-12-07 17:17:13 -0600

    Refactor tax roll data sources and update schema resolution for improved clarity and consistency

--- models/staging/tax_roll/stg_tax_roll_schema_resolve.sql
+++ models/staging/tax_roll/stg_tax_roll_schema_resolve.sql
@@ -3,19 +3,19 @@
 ) }}
 
 select
-    coalesce(parcel, "PROP ID")::varchar(32) as parcel_id,
-    coalesce(tax_year, "Tax Year", "TAX YEAR")::int as tax_year,
-    coalesce(address, "Location", "PROP LOCATION")::varchar(64) as parcel_address,
-    coalesce(assessed_value_land::varchar, "Assessed Value Land", "LAND ASSESSMENT")::numeric(18,2) as assessed_value_land,
-    coalesce(assessed_value_improvement::varchar, "Assessed Value Improvement", "IMPR ASSESSMENT")::numeric(18,2) as assessed_value_improvement,
-    coalesce(total_assessed_value::varchar, "Total Assessed Value", "TOTAL ASSESSMENT")::numeric(18,2) as total_assessed_value,
-    coalesce(est_fair_mkt_land::varchar, "Est. Fair Mkt. Land", "EST FMV LAND")::numeric(18,2) as est_fair_mkt_land,
-    coalesce(est_fair_mkt_imp::varchar, "Est. Fair Mkt. Improvement", "EST FMV IMPR")::numeric(18,2) as est_fair_mkt_improvement,
-    coalesce("Total Estimated Fair Market", "EST FMV TOTAL")::numeric(18,2) as total_estimated_fair_market,
-    coalesce("Current County Net Tax", "COUNTY TAX")::numeric(18,2) as current_county_net_tax,
-    coalesce("Current City Net Tax", "CITY TAX")::numeric(18,2) as current_city_net_tax,
-    coalesce("Current School Net Tax", "SCHOOL TAX")::numeric(18,2) as current_school_net_tax,
-    coalesce("Current MATC Net Tax", "MATC TAX")::numeric(18,2) as current_matc_net_tax,
-    coalesce(cur_net_total::varchar, "Total Current  Net Tax", "NET TAX")::numeric(18,2) as total_current_net_tax,
-    coalesce(cur_total::varchar, "Total Current Tax", "GROSS TAX")::numeric(18,2) as total_current_tax
-from {{ ref('stg_tax_roll_union') }}
+    coalesce(parcel, prop_id)::varchar(32) as parcel_id,
+    tax_year::int as tax_year,
+    coalesce(location, prop_location)::varchar(64) as parcel_address,
+    coalesce(assessed_value_land, land_assessment)::numeric(18,2) as assessed_value_land,
+    coalesce(assessed_value_improvement, impr_assessment)::numeric(18,2) as assessed_value_improvement,
+    coalesce(total_assessed_value, total_assessment)::numeric(18,2) as total_assessed_value,
+    coalesce(est_fair_mkt_land, est_fmv_land)::numeric(18,2) as est_fair_mkt_land,
+    coalesce(est_fair_mkt_improvement, est_fmv_impr)::numeric(18,2) as est_fair_mkt_improvement,
+    coalesce(total_estimated_fair_market, est_fmv_total)::numeric(18,2) as total_estimated_fair_market,
+    coalesce(current_county_net_tax, county_tax)::numeric(18,2) as county_tax,
+    coalesce(current_city_net_tax, city_tax)::numeric(18,2) as city_tax,
+    coalesce(current_school_net_tax, school_tax)::numeric(18,2) as school_tax,
+    coalesce(current_matc_net_tax, matc_tax)::numeric(18,2) as matc_tax,
+    coalesce(total_current__net_tax, net_tax)::numeric(18,2) as net_tax,
+    coalesce(total_current_tax, gross_tax)::numeric(18,2) as gross_tax
+from {{ source('webfiles', 'madison_tax_roll') }}
--- models/staging/tax_roll/stg_tax_roll_union.sql
+++
@@ -1,7 +0,0 @@
-{{ config(
-    tags=['tax_roll']
-) }}
-
-select * from {{ source('arcgis','madison_tax_roll') }}
-union all by name
-select * from {{ source('legacy','madison_tax_roll_legacy') }}
