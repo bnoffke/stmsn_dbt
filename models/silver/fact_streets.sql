97718d9e62cf0e0bd45c22b894b51f0ff449656f
Ben Noffke <bnoffke3790@gmail.com>
2025-12-10 13:03:18 -0600

    Add madison_crs variable and implement fact_streets model for street data integration

--- models/silver/fact_streets.sql
+++ models/silver/fact_streets.sql
@@ -1,9 +1,9 @@
 {{ config(
-    tags=['parcels'],
+    tags=['streets'],
     location=get_external_location()
 ) 
 }}
 
 select *,
     st_transform(geom, '{{ var("madison_crs") }}', 'EPSG:4326') as geom_4326
-from {{ ref('stg_parcels_fix_lot_size') }}
+from {{ ref('stg_streets') }}
---
+++ models/staging/streets/stg_streets.sql
@@ -0,0 +1,22 @@
+{{ config(
+    tags=['streets']
+)}}
+
+select 
+    asset_id as street_id,
+    year as street_year,
+    geometry as geom,
+    speed_limit,
+    surface_width as street_width,
+    lanes as street_lanes,
+    shape_st_lengthx as street_length,
+    truck_route as is_truck_route,
+    case when sidewalk in ('1','2','3') then 1 else 0 end as has_sidewalk,
+    median_width,
+    case when oneway::int > 0 then 1 else 0 end as is_oneway,
+    case when city_maintains = 'Yes' then 1
+         when maintained_by = 'MAD-C' then 1
+         else 0 end city_maintains,
+    ald_dist as alder_district,
+
+from {{ source('arcgis','madison_streets') }}
