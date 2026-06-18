7c7df54e7b43432aae3d59852463dde8a210229d
Ben Noffke <bnoffke3790@gmail.com>
2026-06-18 10:36:17 -0500

    Buffer + select largest polygon of multi in district 4 and 8 to take care of lake overlap

--- models/staging/alder_districts/stg_alder_districts.sql
+++ models/staging/alder_districts/stg_alder_districts.sql
@@ -14,7 +14,7 @@ with source as (
 mendota as (
     select st_buffer(
         st_transform(st_flipcoordinates(st_makevalid(geometry)), 'EPSG:4326', '{{ var("madison_crs") }}'),
-        8
+        3.281
     ) as geom
     from {{ source('legacy','madison_lakes_and_rivers') }}
     where objectid = 132
@@ -23,19 +23,44 @@ mendota as (
 monona as (
     select st_buffer(
         st_transform(st_flipcoordinates(st_makevalid(geometry)), 'EPSG:4326', '{{ var("madison_crs") }}'),
-        8
+        3.281
     ) as geom
     from {{ source('legacy','madison_lakes_and_rivers') }}
     where objectid = 131
+),
+
+differences as (
+    select
+        alder_district_id,
+        alder_district_name,
+        alder_district_sqft,
+        geom,
+        case
+            when alder_district_id = '8' then st_difference(geom, (select geom from mendota))
+            when alder_district_id = '4' then st_difference(geom, (select geom from monona))
+            else geom
+        end as diff_geom
+    from source
+),
+
+largest_part as (
+    select
+        alder_district_id,
+        part.geom as display_geom
+    from differences,
+        unnest(st_dump(diff_geom)) as t(part)
+    where alder_district_id in ('4', '8')
+    qualify row_number() over (
+        partition by alder_district_id
+        order by st_area(part.geom) desc
+    ) = 1
 )
 
-select alder_district_id,
-    alder_district_name,
-    alder_district_sqft,
-    geom,
-    case
-        when alder_district_id = '8' then st_difference(geom, (select geom from mendota))
-        when alder_district_id = '4' then st_difference(geom, (select geom from monona))
-        else geom
-    end as display_geom
-from source
+select
+    d.alder_district_id,
+    d.alder_district_name,
+    d.alder_district_sqft,
+    d.geom,
+    coalesce(lp.display_geom, d.geom) as display_geom
+from differences d
+left join largest_part lp on d.alder_district_id = lp.alder_district_id
